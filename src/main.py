ce2ef22cf9d16926d9b183aa7f6564e8f047d50b
Daniel <Daniel.Milholland@icloud.com>
2016-05-17 21:13:34 -0500

    Added Row Keys

--- src/main.py
+++ src/main.py
@@ -1,15 +1,55 @@
 from openpyxl import load_workbook
 
 # Load the test file
-wb = load_workbook('../example/Example.xlsx')
+wb = load_workbook('example/Example.xlsx')
 # Get worksheet
 try:
     ws = wb['Data']
-    for row in ws.rows:
-        for cell in row:
-           print(cell.value)
-        
 except:
     print("Could not find a Sheet in excel with the name Data.")
+    raise
 
+def get_row_data(row):
+    row_array = []
+    for cell in row:
+        row_array.append(str(cell.value).strip())
+    return row_array
 
+def main():
+    keys = get_row_data(ws.rows[0])
+    values = get_row_data(ws.rows[1])
+    data = dict(zip(keys, values))
+    
+    # Key values
+    # Reference
+    # AWB
+    # Client
+    # Shipper
+    # Consignee
+    # Airline
+    # Forwarder
+    # Ultimate Destination
+    # To
+    # Flight
+    # Via-1
+    # Carrier-1
+    # Via-2
+    # Carrier-2
+    # Accounting
+    # Handling
+    # Pieces
+    # Weight
+    # Chargeable
+    # Unit Price
+    # L
+    # W
+    # H
+    # Additional
+    # Ammount
+    # Agent
+    # Customs
+    # Freight
+    # Insurance
+    # Date
+main()
+    
